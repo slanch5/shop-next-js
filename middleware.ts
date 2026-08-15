5aa3bfecff11a21b0de72fae8207fefae3119305
slanch5 <bvelorant@gmail.com>
2026-08-15 23:07:53 +0300

    fix path v2

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,18 @@
-import {
-  auth,
-  clerkMiddleware,
-  createRouteMatcher,
-} from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher(["/", "/products(.*)", "/about"]);
 const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
-export default clerkMiddleware((auth, req) => {
-  const isAdminUser = auth().userId === process.env.ADMIN_USER;
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+  const isAdminUser = userId === process.env.ADMIN_USER;
+
   if (isAdminRoute(req) && !isAdminUser) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
-  if (!isPublicRoute(req)) auth().protect();
+  if (!isPublicRoute(req)) await auth.protect();
 });
 
 export const config = {
--- utils/actions.ts
+++ utils/actions.ts
@@ -361,7 +361,7 @@ export const findExistingReview = async (userId: string, productId: string) => {
 };
 
 export const fetchCartItems = async () => {
-  const { userId } = auth();
+  const { userId } = await auth();
   const cart = await db.cart.findFirst({
     where: {
       clerkId: userId ?? "",
