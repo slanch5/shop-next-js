4a1eae5f0e76aae295d9b08f4c37572566068617
slanch5 <bvelorant@gmail.com>
2026-08-15 23:48:31 +0300

    fix path v4

--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,25 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher(["/", "/products(.*)", "/about"]);
+
 const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
+
   const isAdminUser = userId === process.env.ADMIN_USER;
 
+  // Якщо користувач заходить в адмінку
+  // і він не є адміністратором — повертаємо на головну
   if (isAdminRoute(req) && !isAdminUser) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
-  if (!isPublicRoute(req)) await auth.protect();
+  // Якщо сторінка не є публічною —
+  // користувач повинен бути авторизований
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
 });
 
 export const config = {
